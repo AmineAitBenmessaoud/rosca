b569219218052e13eefee8e895bf65d8d48a8d4a
Amine <amineabm52002@gmail.com>
2023-06-19 14:52:51 +0100

    adding post page

---
+++ signin.php
@@ -0,0 +1,118 @@
+<?php include('server.php') ?>
+<?php 
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: index.php');
+	}
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: index.php");
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create an account</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+	<?php include('head.php') ?>
+</head>
+</html>
+<body>
+	<?php include('bar.php') ?>
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+				<form class="login100-form validate-form flex-sb flex-w" method="post" action="pass">
+					<span class="login100-form-title p-b-53">
+						Changer le mot de passe
+					</span>			
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Ancien mot de passe
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password_1" >
+						<span class="focus-input100"></span>
+					</div>
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Nouveau mot de passe
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password_2" >
+						<span class="focus-input100"></span>
+					</div>
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Confirmer le nouveau mot de passe
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password_3" >
+						<span class="focus-input100"></span>
+					</div>
+					<?php include('errors.php'); ?>		
+					<div class="container-login100-form-btn m-t-17">
+						<button class="login100-form-btn" name="ch_pass" type="submit">
+							Enregister
+						</button>
+					</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+	 <footer id="footer" style="background-color: #314759; color: white; position: relative;">
+    <div class="container text-center">
+      <small>Copyright &copy; <a href="../" style="color: white;">Cpge Kingdom</a><b> version 1.0</b></small>
+    </div>
+  </footer>
+</body>
+</html>
